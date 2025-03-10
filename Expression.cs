87eb7bca86955d2707d043d10162777928fc6343
Matt Ferrero <mattaferrero@users.noreply.github.com>
2025-03-10 17:35:13 -0400

    Added PullStringChunks class for arbitrary substring retrieval.

---
+++ Expression.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mammon
+{
+    // ideas: Solve(), Rewrite(), Factor(), Graph() etc.
+    public class Expression {
+        private readonly string _expression;
+
+        public Expression(string expression) {
+            if (string.IsNullOrEmpty(expression)) {
+                throw new ArgumentException("Passing valid string object to class Expression is required.");
+            }
+
+            _expression = expression;
+        }
+
+    }
+}
