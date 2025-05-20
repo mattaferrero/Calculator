4f5975a1fb2398d3a46b1b868864f4c5064745e0
Matt Ferrero <mattaferrero@users.noreply.github.com>
2025-05-20 13:21:14 -0400

    Added TokenScans.cs

---
+++ src/TokenScans.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) FAC. All rights reserved.
+// Mammon's Calculator
+
+// This class is just used to keep track of the input string information:
+// last index accessed, remaining subsets etc.
+using System;
+
+namespace Mammon {
+    public class TokenScan {
+        private string _input;
+
+        // Constructors
+        public TokenScan(string input) {
+            _input = input;
+        }
+
+        // Properties
+        public int Offset { get; set; } = 0;
+
+        public string Str {
+            get {  return _input; }
+            set { _input = value; }
+        }
+
+        public char GetCurrentChar {
+            get { return Str[Offset]; }
+        }
+
+        public string GetRemainingInput {
+            get { return Str.Substring(Offset); }
+        }
+    }
+}
