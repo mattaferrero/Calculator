23fa857d95e257bcbbb0dd2ad8d344dea1a12d1d
Matt Ferrero <mattaferrero@users.noreply.github.com>
2025-05-27 16:22:22 -0400

    junk commit: sender error in button handler

--- src/Form1.cs
+++ src/Form1.cs
@@ -17,45 +17,18 @@ namespace Mammon {
         public Form1() {
             InitializeComponent();
             _tokenStream = new TokenStream("Avaricia");
+
+            // Numeric button handler
+            button0.Click += NumpadAppend_Click();
+        }
+
+        private void NumpadAppend_Click(object sender, EventArgs e) {
+            if (sender is Button clickedButton) {
+                textBox1.Text += clickedButton.Text;
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-
-        }
-
-        private void button0_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button3_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button5_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button7_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button8_Click(object sender, EventArgs e) {
-
-        }
-
-        private void button9_Click(object sender, EventArgs e) {
 
         }
 
